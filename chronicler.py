370b9735b8c87aabf277fe518659aadaa1b8d82f
Garth Mortensen <mortensengarth@hotmail.com>
2024-12-24 08:19:12 -0600

    Docs: Add class docstring

--- chronicler.py
+++ chronicler.py
@@ -5,6 +5,17 @@ import sys
 
 
 class Chronicler:
+    """
+    A logging utility class to initialize and manage logging config.
+
+    Writes to both `stdout` (for  aws cloudwatch) and a timestamped log file in `./logs`.
+
+    Attributes:
+        log_file (str): The path to the log file being written.
+    
+    Methods:
+        __init__(script_path): Initializes the logging  config.
+    """
     def __init__(self, script_path):
         # for log filename and print filepath
         script_name = os.path.splitext(os.path.basename(script_path))[0]
@@ -12,13 +23,14 @@ class Chronicler:
         timestamp = time.strftime("%Y%m%d_%H%M%S", time.localtime())
         self.log_file = f"./logs/{timestamp}_{script_name}.log"
 
+        # ensure exists
         os.makedirs("./logs", exist_ok=True)
 
-        handlers = [  # write stdout and errout
+        handlers = [
             # aws cloudwatch monitors stdout and stderr and can auto pick them up
             logging.StreamHandler(sys.stdout),  
-            logging.FileHandler(filename=self.log_file, mode="w"),
-        ]  # write to file
+            logging.FileHandler(filename=self.log_file, mode="w"),  # write to file
+        ]
 
         # 20240202_154449 INFO practice.py:56 log_meta| pid:    10629
         log_format = (
