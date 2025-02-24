bed13bebe1fa3a7a4c93a37b71c7ec511f3d857b
Garth Mortensen <mortensengarth@hotmail.com>
2025-02-23 20:15:58 -0600

    Rm: Remove old script

--- src/model.py
+++
@@ -1,9 +0,0 @@
-import os
-import pandas as pd
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
-series_1_path = os.path.join(script_dir, "../input/data/series_1.csv")
-series_2_path = os.path.join(script_dir, "../input/data/series_2.csv")
-series_1 = pd.read_csv(series_1_path)
-series_2 = pd.read_csv(series_2_path)
